30f0d0b49063505d13d2969a006443983aab6099
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2023-07-04 18:42:05 +0300

    JavaScript - Exercises 1.1 & 1.2 (7.4.23)

---
+++ As-Submits-JavaScript/exercise-1.1/exercise_1.1.js
@@ -0,0 +1,16 @@
+let message = "You enter a dimly lit room";
+let treasureCount = 10;
+let key = {goldenKey: "intricate carvings"};
+let isTrapActivated = true;
+let secretPassage = null;
+let mysteriousArtifact
+let ancientSymbol = Symbol('stoneTablet');
+
+console.log(typeof(message));
+console.log(typeof(treasureCount));
+console.log(typeof(key));
+console.log(typeof(isTrapActivated));
+console.log(typeof(secretPassage));
+console.log(typeof(mysteriousArtifact));
+console.log(typeof(ancientSymbol));
+
